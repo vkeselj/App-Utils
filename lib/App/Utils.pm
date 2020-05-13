9e3bcc507f813161a61f0c0c321848802cb9d162
Vlado Keselj <vlado@dnlp.ca>
2020-05-13 16:56:43 -0300

    Initial commit

---
+++ lib/App/Utils.pm
@@ -0,0 +1,23 @@
+package App::Utils;
+
+our $VERSION = 0.01;
+
+=head1 NAME
+
+App::Utils - an app that contain some useful command-line utilities
+
+=head1 DESCRIPTION
+
+This is a stub module, see F<date-tag> for detail of a utility.
+
+=head1 AUTHOR
+
+Vlado Keselj
+
+=head1 LICENSE
+
+Artistic License L<perlartistic>
+
+=cut
+
+1;
