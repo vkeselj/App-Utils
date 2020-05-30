064ee1f3bfa9d7c11964b08f1b216069de8deba5
Vlado Keselj <vlado@dnlp.ca>
2020-05-30 13:31:49 -0300

    Version 0.042

--- lib/App/Utils.pm
+++ lib/App/Utils.pm
@@ -1,6 +1,6 @@
 package App::Utils;
 
-our $VERSION = 0.041;
+our $VERSION = 0.042;
 
 =head1 NAME
 
@@ -9,21 +9,30 @@ App::Utils - some useful command-line utilities
 =head1 DESCRIPTION
 
 This is a stub module that contains some useful command-line utilities,
-created for Linux environment.
+created for Linux environment.  Detailed descriptions are included in
+the programs.
 
 F<date-tag> - print a date-time tag in form C<YYYY-MM-DD-hhmmss>
 
-F<remove-empty-dirs> - removes empty directories recursively
+F<remove-empty-dirs> - remove recursively empty directories
 
-F<save> saves a snapshot of files in the directory "saved.d".
+F<save> - save a snapshot of given files in C<saved.d> directory
 
 =head1 AUTHOR
 
 Vlado Keselj <vlado@dnlp.ca> 2020
 
-=head1 LICENSE
+=head1 INSTALLATION
 
-Artistic License L<perlartistic>
+Using C<cpan>:
+
+ $ cpan App::Utils
+
+Manual install:
+
+ $ perl Makefile.PL
+ $ make
+ $ make install
 
 =cut
 
